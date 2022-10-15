caa308682cb1ac68ca34d733c1661686fe7f3a1d
Habiba Anika <anikahabiba49@gmail.com>
2022-10-15 17:51:35 +0600

    null Vs undefined

---
+++ null VS undefind.js
@@ -0,0 +1,19 @@
+let pakhi;
+function add(num1, num2) {
+    console.log(num1+num2);
+    return
+}
+function add(num1, num2) {
+  console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+
+const tiktokerNo1 = {name:"Sabbir",phone:0177789456}
+console.log(tiktokerNo1.number);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
